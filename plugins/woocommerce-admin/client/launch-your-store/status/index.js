4cd792cdc881b858c128deccc3bd2c4607be630d
rjchow <me@rjchow.com>
2024-04-15 09:24:46 +1000

    add site visibility flag

--- plugins/woocommerce-admin/client/launch-your-store/status/index.js
+++ plugins/woocommerce-admin/client/launch-your-store/status/index.js
@@ -63,7 +63,12 @@ export const LaunchYourStoreStatus = ( { comingSoon, storePagesOnly } ) => {
 								<MenuItem
 									onClick={ () => {
 										recordEvent(
-											'launch_your_store_badge_menu_manage_site_visibility_click'
+											'launch_your_store_badge_menu_manage_site_visibility_click',
+											{
+												site_visibility: isComingSoon
+													? 'coming_soon'
+													: 'live',
+											}
 										);
 									} }
 									href={ getAdminLink(
